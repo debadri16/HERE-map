fbe491822f26e150c6262437b78bf485671aa2dc
debadri16 <debadri16@gmail.com>
2019-04-20 18:50:33 +0530

    fetch shop details into item comp

--- src/app/home/list/items/items.component.ts
+++ src/app/home/list/items/items.component.ts
@@ -10,13 +10,19 @@ export class ItemsComponent implements OnInit {
   @Input() data : any;
 
   public title : any;
+  public type: any;
+  public phn: any;
+  public rating: any;
 
   constructor() { }
 
   ngOnInit() {
-//    console.log(this.data);
+   console.log(this.data);
 
-//    this.title = this.data['name'];
+   this.title = this.data['name'];
+   this.type = this.data['type'];
+   this.phn = this.data['phoneNumber'];
+   this.rating = this.data['rating'];
   }
 
 }
